4a322b31d2482f36a423a2be006f50c19038206e
alvaro-gargon <alvaro.garcia.daw@gmail.com>
2026-05-12 08:21:45 +0200

    fix - fix a problem wich caused to return error 404 at refreshing

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,11 @@ Route::inertia('/', 'Welcome', [
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
 });
+/*Metodo añadido para controlar el movimiento entre paginas sin que ocurra errores.
+La pagina siempre se dirige a la vista principal independientemente de lo que ponga en la URI
+*/
+Route::get('/{any}', function () {
+    return view('app'); 
+})->where('any', '.*');
 
 require __DIR__.'/settings.php';
